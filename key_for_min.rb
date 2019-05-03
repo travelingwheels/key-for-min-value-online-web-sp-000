41a74b20eef6ca0fd5befe7cd4f219a0e54b7635
Gary Wheeler <gkwheeler40@gmail.com>
2019-05-03 14:49:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,11 @@
 def key_for_min_value(name_hash)
     low_value = 0 
     low_key = nil
-    name_hash.collect do |key, value|
+    name_hash.each do |key, value|
       if low_value == 0 || value < low_value
         low_value = value 
         low_key = key
       end
     end
-    
-
+    low_key
 end
